525530cd4a064900026b623fd9fe66ac7d9efd2a
martincosimano <martincosimano@hotmail.com>
2023-05-10 18:01:58 -0300

    changes: -setting up the app, there are 9 slots that generates a random number between 1-6. -added a roll button that rolls all the numbers when clicked. -made some initial styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Dice from './components/Dice'
 
 export default function App() {
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 9; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Dice value={die} />)
+
+
   return (
-    <div>
-      <h1>Tenzies</h1>
-    </div>
+    <main>
+      <div className="dice-container">
+        {diceElements}
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
+      </div>
+    </main>
   );
 }
