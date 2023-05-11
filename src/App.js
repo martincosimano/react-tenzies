1fc93c65c72416948a30f8f8dfda9c15802e2127
martincosimano <martincosimano@hotmail.com>
2023-05-11 11:51:09 -0300

    changes: -modified styling. -added confetti package and a win condition. -when winning can restart the game clicking the button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Dice from './components/Dice'
+import Confetti from 'react-confetti'
 
 export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if(allHeld && allSameValue) {
+      setTenzies(true)
+    }
+  }, [dice])
 
   function generateNewDie() {
     return {
@@ -15,17 +26,22 @@ export default function App() {
 
   function allNewDice() {
     const newDice = []
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 10; i++) {
       newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice() {
-    setDice(prevDice => prevDice.map(die => {
-      return die.isHeld ? die : generateNewDie()
-    }))
-  }
+    if(!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+      }
+    }
 
   function holdDice(id) {
     setDice(prevDice => prevDice.map(die => {
@@ -35,17 +51,20 @@ export default function App() {
 
   const diceElements = dice.map(die => <Dice id={die.id} key={die.id} value={die.value} isHeld={die.isHeld} holdDice={holdDice} />)
 
-
   return (
     <main>
-      <div className="text-container">
-        <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      </div>  
-      <div className="dice-container">
+    {tenzies && <Confetti />}
+    <h1 className="title">Tenzies</h1>
+    <p className="instructions">Roll until all dice show the same value. Click to hold a die.</p>
+    <div className="dice-container">
         {diceElements}
-        <button className="roll-dice" onClick={rollDice}>Roll</button>
-      </div>
-    </main>
+    </div>
+    <button 
+        className="roll-dice" 
+        onClick={rollDice}
+    >
+        {tenzies ? "New Game" : "Roll"}
+    </button>
+</main>
   );
 }
