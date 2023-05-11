ba0e05561b2289967d0520cd4b1259f511feb9bc
martincosimano <martincosimano@hotmail.com>
2023-05-11 10:57:27 -0300

    changes: -now when clicking a die, the value of isHeld property will flip. -when clicking a die and pressing the 'roll' button, the die selected will keep his value. -added a title and a paragraph with instructions.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: Math.ceil(Math.random() * 100000)
+    }
+  }
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 9; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: Math.ceil(Math.random() * 100000)
-      })
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
 
   function holdDice(id) {
     setDice(prevDice => prevDice.map(die => {
-      return die.id === id ? {...die, isHeld: !die.isHeld} : die;
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
     }))
   }
 
@@ -32,6 +38,10 @@ export default function App() {
 
   return (
     <main>
+      <div className="text-container">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>  
       <div className="dice-container">
         {diceElements}
         <button className="roll-dice" onClick={rollDice}>Roll</button>
